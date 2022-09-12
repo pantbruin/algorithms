5e8122da595fd208a98dd867413561c5f0cd5b59
Jesse Pantoja <pantojabruin@gmail.com>
2022-09-12 15:07:51 -0700

    Remove manual tests in binary_search.py

--- python/search-algorithms/binary_search.py
+++ python/search-algorithms/binary_search.py
@@ -16,9 +16,3 @@ def binary_search(li, target, is_sorted=False):
             right_ptr = mid_index - 1
         else:
             left_ptr = mid_index + 1
-
-
-even = [0, 1, 2, 3, 4, 5, 6, 7]
-odd = [1, 2, 3, 4, 5]
-
-binary_search(even, 1, True)
